bb056d2856786d5a3b65889c7c7f04bd4a3d3af5
thisisaaronland <devnull@localhost>
2020-04-14 14:53:07 -0700

    stub out sfomhandlers

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/sfomuseum/go-www-geotag-sfomuseum
 go 1.12
 
 require (
+	github.com/aaronland/go-http-rewrite v0.0.4
 	github.com/sfomuseum/go-www-geotag v0.0.0-20200414212401-c0ef531264d8
 	github.com/sfomuseum/go-www-geotag-whosonfirst v0.0.0-20200414212615-c0d3f9292cd0
 	github.com/whosonfirst/go-bindata v0.1.0
---
+++ sfomuseum.go
@@ -0,0 +1,125 @@
+package sfomuseum
+
+import (
+	"github.com/aaronland/go-http-rewrite"
+	_ "log"
+	"net/http"
+	"path/filepath"
+	"strings"
+)
+
+type ResourceOptions struct {
+	JS  []string
+	CSS []string
+}
+
+func DefaultEditorOptions() *ResourceOptions {
+
+	opts := &ResourceOptions{
+		CSS: []string{},
+		JS: []string{
+			"/javascript/sfomuseum.maps.js",
+			"/javascript/sfomuseum.geotag.init.js",
+		},
+	}
+
+	return opts
+}
+
+func AppendResourcesHandler(next http.Handler, opts *ResourceOptions) http.Handler {
+
+	return AppendResourcesHandlerWithPrefix(next, opts, "")
+}
+
+func AppendResourcesHandlerWithPrefix(next http.Handler, opts *ResourceOptions, prefix string) http.Handler {
+
+	js := opts.JS
+	css := opts.CSS
+
+	if prefix != "" {
+
+		for i, path := range js {
+			js[i] = appendPrefix(prefix, path)
+		}
+
+		for i, path := range css {
+			css[i] = appendPrefix(prefix, path)
+		}
+	}
+
+	ext_opts := &rewrite.AppendResourcesOptions{
+		JavaScript:  js,
+		Stylesheets: css,
+	}
+
+	return rewrite.AppendResourcesHandler(next, ext_opts)
+}
+
+/*
+func AssetsHandler() (http.Handler, error) {
+
+	fs := assetFS()
+	return http.FileServer(fs), nil
+}
+
+func AssetsHandlerWithPrefix(prefix string) (http.Handler, error) {
+
+	fs_handler, err := AssetsHandler()
+
+	if err != nil {
+		return nil, err
+	}
+
+	prefix = strings.TrimRight(prefix, "/")
+
+	if prefix == "" {
+		return fs_handler, nil
+	}
+
+	rewrite_func := func(req *http.Request) (*http.Request, error) {
+		req.URL.Path = strings.Replace(req.URL.Path, prefix, "", 1)
+		return req, nil
+	}
+
+	rewrite_handler := rewrite.RewriteRequestHandler(fs_handler, rewrite_func)
+	return rewrite_handler, nil
+}
+
+func AppendAssetHandlers(mux *http.ServeMux) error {
+	return AppendAssetHandlersWithPrefix(mux, "")
+}
+
+func AppendAssetHandlersWithPrefix(mux *http.ServeMux, prefix string) error {
+
+	asset_handler, err := AssetsHandlerWithPrefix(prefix)
+
+	if err != nil {
+		return nil
+	}
+
+	for _, path := range AssetNames() {
+
+		path := strings.Replace(path, "static", "", 1)
+
+		if prefix != "" {
+			path = appendPrefix(prefix, path)
+		}
+
+		mux.Handle(path, asset_handler)
+	}
+
+	return nil
+}
+*/
+
+func appendPrefix(prefix string, path string) string {
+
+	prefix = strings.TrimRight(prefix, "/")
+
+	if prefix != "" {
+		path = strings.TrimLeft(path, "/")
+		path = filepath.Join(prefix, path)
+	}
+
+	return path
+}
