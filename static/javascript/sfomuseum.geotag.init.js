0b86aa64d79dab1637ac18068c82d41734815d17
thisisaaronland <devnull@localhost>
2020-05-13 12:19:49 -0700

    custom save button onclick

--- static/javascript/sfomuseum.geotag.init.js
+++ static/javascript/sfomuseum.geotag.init.js
@@ -30,4 +30,41 @@ window.addEventListener("load", function load(event){
 	    console.log("Error:", message);
 	}
     };
+
+    var save = document.getElementById("writer-save");
+
+    if (save){
+
+	save.onclick = function(e){
+
+	    var camera = geotag.camera.getCamera();
+	    
+            if (! camera){
+		console.log("Unable to retrieve camera");
+		return false;
+	    }
+	    
+            var uri = document.body.getAttribute("data-geotag-uri", uri);
+	    
+	    if (! uri){
+		console.log("Missing data-geotag-uri attribute");
+		return false;
+	    }
+	    
+            var fov = camera.getFieldOfView();
+	    
+	    var on_success = function(rsp){
+		console.log("WRITE OKAY", rsp);
+		console.log("WEBKIT IT UP");
+	    };
+	    
+            var on_error = function(err){
+		console.log("WRITE ERROR", err);
+	    };
+	    
+            geotag.writer.write_geotag(uri, fov, on_success, on_error);
+	    return false;
+	};
+    }
+    
 });
